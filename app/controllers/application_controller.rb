7d802bcc484570dccda9901b3a8cb6bc777329e8
pedro meireles <pedro.carvalho@prevision.com.br>
2023-06-09 00:54:37 -0300

    wip - adding some validation at user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_costumer
+
+  def authenticate_costumer
+    token = extract_token
+
+    raise SecurityError, 'Unauthorized' if token.nil?
+
+    @current_costumer = Costumer.find_by(token: extract_token)
+  end
+
+  private
+
+  def extract_token
+    token = request.headers['Authorization']&.split(' ')
+
+    return nil unless token
+
+    token[1]
+  end
 end
