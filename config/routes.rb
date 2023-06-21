5b2da3d3123269f8b3e0548dc13b571b5631662a
pedro meireles <pedro.carvalho@prevision.com.br>
2023-06-20 21:56:07 -0300

    add producs controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/', to: 'home#index'
 
   resources :costumers, only: [:index, :create, :show, :update, :destroy]
+  resources :products
 
   post '/login', to: 'login#login'
   post '/validate', to: 'login#validate'
