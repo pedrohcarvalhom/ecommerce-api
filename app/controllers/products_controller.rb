5815ef76b9a386a738d5d1bb75732eb6bb065482
pedro meireles <pedro.carvalho@prevision.com.br>
2023-06-20 22:41:31 -0300

    created repository and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,31 @@
 class ProductsController < ApplicationController
+  def index
+    render json: repository.all, status: 200
+  end
+
+  def show
+    #
+  end
+
+  def create
+    #
+  end
+
+  def update
+    #
+  end
+
+  def destroy
+    #
+  end
+
+  private
+
+  def products_params
+    params.permit(:id, :title, :subtitle, :number_of_reviews, :price, :quantity, :description, :colors)
+  end
+
+  def repository
+    ProductsRepository
+  end
 end
---
+++ app/repositories/products_repository.rb
@@ -0,0 +1,33 @@
+class ProductsRepository
+  class << self
+    def create_product(*attrs)
+      entity.create!(attrs)
+    end
+
+    def get_product(email)
+      entity.find_by_email(email)
+    end
+
+    def find(id)
+      entity.find(id)
+    end
+
+    def destroy(id)
+      entity.destroy(id)
+    end
+
+    def update(attrs)
+      entity.where(id: attrs['id']).update(attrs)
+    end
+
+    def all
+      entity.all
+    end
+
+    private
+
+    def entity
+      Product
+    end
+  end
+end
