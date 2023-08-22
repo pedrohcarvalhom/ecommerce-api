ffa1be0fff845d16ccd117f73eebab1bcfeec208
pedro meireles <pedro.carvalho@prevision.com.br>
2023-08-21 22:04:05 -0300

    WIP - Adding order model

---
+++ app/models/domains/order.rb
@@ -0,0 +1,16 @@
+module Domains
+  class Order
+    attr_reader :delivery_date, :total_cost, :shipping_cost, :quantity
+
+    def initialize(delivery_date, total_cost, shipping_cost, quantity)
+      @delivery_date = delivery_date
+      @total_cost = total_cost
+      @shipping_cost = shipping_cost
+      @quantity = quantity
+    end
+
+    def delivery_valid?
+      raise ArgumentError, I18n.t('custom_errors.orders.delivery_date') unless delivery_date.to_datetime > DateTime.now
+    end
+  end
+end
